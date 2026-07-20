a209bdd6ed05165b994936a68c0cfc2990d8eb73
Arni Tiwari <arni027btcse25@igdtuw.ac.in>
2026-07-20 09:12:25 +0530

    Update evaluation scripts to use renamed best_baseline.pt

--- scripts/evaluate_enhanced.py
+++ scripts/evaluate_enhanced.py
@@ -4,5 +4,5 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 from ultralytics import YOLO
 
-model = YOLO(r"C:\Users\arnit\OneDrive\Desktop\coding projects\underwater-debris-yolov8-gan\models\best.pt")
+model = YOLO(r"C:\Users\arnit\OneDrive\Desktop\coding projects\underwater-debris-yolov8-gan\models\best_baseline.pt")
 metrics = model.val(data=r"C:\Users\arnit\OneDrive\Desktop\coding projects\underwater-debris-yolov8-gan\data\data_enhanced.yaml")
